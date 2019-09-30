50c563eb47e6895c58895acd648f38e2f465ed5f
Franco Marini <franco-marini@outlook.com>
2019-09-30 14:49:46 -0300

    VA: Routing and history mode

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import AppLayot from './theme/Layout.vue'
+import router from './router'
 
 const app = new Vue({
+  router,
   ...AppLayot
 })
 
-export { app }
+export { app, router }
