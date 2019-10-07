7a31be3e6ee3be952f3f28036fd2c5c6d6b22333
Franco Marini <franco-marini@outlook.com>
2019-10-07 16:08:37 -0300

    VA: Server-side rendering and Bundle Renderer

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const fs = require('fs')
 const path = require('path')
 const port = process.env.PORT || 3000
+const { createBundleRenderer } = require('vue-server-renderer')
+let renderer
 
 const indexHTML = (() => {
   return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
@@ -10,11 +12,19 @@ const indexHTML = (() => {
 
 app.use('/dist', express.static(path.resolve(__dirname, './dist')))
 
-require('./build/dev-server')(app)
+require('./build/dev-server')(app, bundle => {
+  renderer = createBundleRenderer(bundle)
+})
 
 app.get('*', (req, res) => {
-  res.write(indexHTML)
-  res.end()
+  renderer.renderToString({ url: req.url }, (err, html) => {
+    if (err) {
+      return res.status(500).send('Server Error')
+    }
+    html = indexHTML.replace('{{ APP }}', html)
+    res.write(html)
+    res.end()
+  })
 })
 
 app.listen(port, () => {
